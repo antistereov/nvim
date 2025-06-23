1cd8bf858ff705dff01fc7901c6e3f52efb3d43b
antistereov <andre.antimonov@stereov.io>
2025-06-23 21:00:40 +0200

    feat(plugins): add autocompletion

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,7 +1,7 @@
 return {
 	"mason-org/mason-lspconfig.nvim",
 	opts = {
-		ensure_installed = { "lua_ls, rust_analyzer", "ts_ls" },
+		ensure_installed = { "lua_ls", "rust_analyzer", "ts_ls" },
 	},
 	dependencies = {
 		{ "mason-org/mason.nvim", opts = {} },
@@ -14,5 +14,17 @@ return {
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        local lspconfig = require('lspconfig')
+
+        lspconfig.ts_ls.setup({
+            capabilities = capabilities
+        })
+        lspconfig.lua_ls.setup({
+            capabilities = capabilities
+        })
+        lspconfig.rust_analyzer.setup({
+            capabilities = capabilities
+        })
 	end,
 }
