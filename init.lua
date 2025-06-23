e3ec4d990bb27c4c6f01b4b91178a52a771ef6c6
antistereov <andre.antimonov@stereov.io>
2025-06-23 15:57:20 +0200

    chore: improve file structure

--- init.lua
+++ init.lua
@@ -1,10 +1,8 @@
 require("config.lazy")
-require("config.theme")
 require("config.keymap")
-require("config.telescope")
-require("config.treesitter")
 
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+vim.cmd("set nowrap")
