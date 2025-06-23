dd0f96880acf086ef8b3ffd7e385705eb51fe1b5
antistereov <andre.antimonov@stereov.io>
2025-06-23 19:20:04 +0200

    feat(plugins): add none-ls formatting

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -1,7 +1,6 @@
 return {
-    'voldikss/vim-floaterm',
-    config = function()
-        vim.keymap.set('n', '<leader>ft', ':FloatermNew<CR>')
-    end
+	"voldikss/vim-floaterm",
+	config = function()
+		vim.keymap.set("n", "<leader>ft", ":FloatermNew<CR>")
+	end,
 }
-
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,22 +1,33 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    branch = 'master',
-    lazy = false,
-    build = ':TSUpdate',
-    config = function()
-        require'nvim-treesitter.configs'.setup {
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "typescript", "kotlin", "java" },
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"markdown",
+				"markdown_inline",
+				"typescript",
+				"kotlin",
+				"java",
+			},
 
-            sync_install = false,
+			sync_install = false,
 
-            auto_install = true,
+			auto_install = true,
 
-            ignore_install = {},
+			ignore_install = {},
 
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-        }
-    end
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+		})
+	end,
 }
