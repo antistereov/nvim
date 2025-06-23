841d65a5c9306a549360bd5def00ad1c8c4fe4f5
antistereov <andre.antimonov@stereov.io>
2025-06-23 23:12:51 +0200

    fix: fix double completion lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,32 +1,18 @@
 return {
 	"mason-org/mason-lspconfig.nvim",
-	opts = {
-		ensure_installed = { "lua_ls", "rust_analyzer", "ts_ls", "yamlls" },
-	},
 	dependencies = {
 		{ "mason-org/mason.nvim", opts = {} },
 		"neovim/nvim-lspconfig",
 	},
 	config = function()
 		require("mason-lspconfig").setup({
-            ensure_installed = { "lua_ls", "rust_analyzer", "ts_ls", "yamlls" }
+            ensure_installed = { "lua_ls", "rust_analyzer", "ts_ls", "yamlls", 	"pyright" }
         })
 
 		vim.diagnostic.config({ virtual_text = true })
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-        local capabilities = require('cmp_nvim_lsp').default_capabilities()
-        local lspconfig = require('lspconfig')
 
-        lspconfig.ts_ls.setup({
-            capabilities = capabilities
-        })
-        lspconfig.lua_ls.setup({
-            capabilities = capabilities
-        })
-        lspconfig.rust_analyzer.setup({
-            capabilities = capabilities
-        })
 	end,
 }
