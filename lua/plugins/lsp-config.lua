3ed16f5c6015b519e42b6dfd2c02571fb50eac4d
Andre Antimonov <andre.antimonov@systema.com>
2025-06-24 10:50:37 +0200

    chore: fix spacing

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,18 +1,17 @@
 return {
-	"mason-org/mason-lspconfig.nvim",
-	dependencies = {
-		{ "mason-org/mason.nvim", opts = {} },
-		"neovim/nvim-lspconfig",
-	},
-	config = function()
-		require("mason-lspconfig").setup({
-            ensure_installed = { "lua_ls", "rust_analyzer", "ts_ls", "yamlls", 	"pyright" , "angularls" }
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+    },
+    config = function()
+        require("mason-lspconfig").setup({
+            ensure_installed = { "lua_ls", "rust_analyzer", "ts_ls", "yamlls", "pyright", "angularls" },
         })
 
-		vim.diagnostic.config({ virtual_text = true })
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-
-	end,
+        vim.diagnostic.config({ virtual_text = true })
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
 }
