7ca37adf80822c2d1777a4d665aed8ea273a3841
antistereov <andre.antimonov@stereov.io>
2025-06-24 22:31:23 +0200

    feat(plugins): add noice ui plugin

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -56,6 +56,7 @@ return {
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
+                    { name = "path" }
 				}, {
 					{ name = "buffer" },
 				}),
